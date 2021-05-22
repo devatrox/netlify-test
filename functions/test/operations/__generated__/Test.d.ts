02b6012a65a1ffe0569897657586af2d3aaacb89
Jason Nickel <mail@devatrox.de>
2021-05-22 12:09:04 +0200

    trying something

---
+++ functions/test/operations/__generated__/Test.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: Test
+// ====================================================
+
+export interface Test_test {
+  id: string;
+  name: string | null;
+}
+
+export interface Test {
+  test: Test_test;
+}
